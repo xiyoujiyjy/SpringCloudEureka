376b89b75fb1c8386468680d023d173872172713
yuanjy <yuanjy@yiche.com>
2018-02-07 17:45:38 +0800

    1

---
+++ Service/OrderServiceOne/Controllers/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderServiceOne.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Order")]
+    public class OrderController : Controller
+    {
+        // GET: api/Order
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "8101", "8101" };
+        }
+
+        // GET: api/Order/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "8101";
+        }
+        
+        // POST: api/Order
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Order/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
